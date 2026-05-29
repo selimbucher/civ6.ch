7bb48b5618dab89cede68e282b346bed256c225f
Selim Bucher <selim.bucher@slimmer.ch>
2026-05-29 23:13:37 +0200

    chore: setup devshell for nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "civ6.ch";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            pnpm
+            go
+          ];
+        };
+      }
+    );
+}
